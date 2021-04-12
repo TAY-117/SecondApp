437446811296efc0926d6c69308891017b4a5452
TAY-117 <belchack@gmail.com>
2021-04-12 17:47:04 +0300

    Добавьте файлы проекта.

---
+++ SecondApp.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+
+		string myName;
+		myName = "Alisa";
+
+		Console.WriteLine(myName);
+
+		Console.ReadKey();
+	}
+}
