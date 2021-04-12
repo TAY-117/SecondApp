97df4a35f236adf47e297d48568c282675a4fc7e
TAY-117 <belchack@gmail.com>
2021-04-12 19:40:11 +0300

    Исправлена ошибка в литерале кода символа #

--- SecondApp.ConsoleApp/Program.cs
+++ SecondApp.ConsoleApp/Program.cs
@@ -13,7 +13,7 @@ class MainClass
 		Console.WriteLine("\t Мне много лет.");
 		Console.WriteLine("\t My name is \n  {0}", МyName);
 		Console.WriteLine('\u0023');//символ # в Hex-коде ASCII
-		Console.WriteLine('\x0035');//символ # в Dec-коде ASCII
+		Console.WriteLine('\x23');  //символ # в Hex-коде ASCII
 		Console.WriteLine((char)35);//преобразование Dec-кода ASCII символ #
 		Console.ReadKey();
 	}
