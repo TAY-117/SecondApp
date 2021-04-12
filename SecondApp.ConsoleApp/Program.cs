37c17f7127275f4500295418c8c63a8dbfdeda5b
TAY-117 <belchack@gmail.com>
2021-04-12 22:39:29 +0300

    Для попробывать добавлены строки кода, где типы данных сообщают о своём диапазоне значений

--- SecondApp.ConsoleApp/Program.cs
+++ SecondApp.ConsoleApp/Program.cs
@@ -13,5 +13,17 @@ class MainClass
 		Console.WriteLine("MyAge " + MyAge);
 		Console.WriteLine("Do I have a pet? " + HaveIApet);
 		Console.WriteLine("My shoe size is " + MyShoeSize);
+
+		//пример получения инфы о диапазоне значений от типа данных
+		Console.WriteLine("byteMin {0} ", byte.MinValue);
+		Console.WriteLine("byteMax {0} ", byte.MaxValue);
+		Console.WriteLine("IntMin {0} ", int.MinValue);
+		Console.WriteLine("IntMax {0} ", int.MaxValue);
+		Console.WriteLine("floatMin {0} ", float.MinValue);
+		Console.WriteLine("floatMax {0} ", float.MaxValue);
+		Console.WriteLine("doubleMin {0} ", double.MinValue);
+		Console.WriteLine("doubleMax {0} ", double.MaxValue);
+
+		Console.ReadKey();
 	}
 }
