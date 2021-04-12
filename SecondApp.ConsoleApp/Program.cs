2305a7945aac18163e04d91676b085e43b2c05b8
TAY-117 <belchack@gmail.com>
2021-04-12 19:26:42 +0300

    Использование литералов Hex и Dec кодов

--- SecondApp.ConsoleApp/Program.cs
+++ SecondApp.ConsoleApp/Program.cs
@@ -12,7 +12,9 @@ class MainClass
 		Console.WriteLine("\t Привет, мир!");
 		Console.WriteLine("\t Мне много лет.");
 		Console.WriteLine("\t My name is \n  {0}", МyName);
-		Console.WriteLine((char)35);
+		Console.WriteLine('\u0023');//символ # в Hex-коде ASCII
+		Console.WriteLine('\x0035');//символ # в Dec-коде ASCII
+		Console.WriteLine((char)35);//преобразование Dec-кода ASCII символ #
 		Console.ReadKey();
 	}
 }
